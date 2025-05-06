7e368bf9f81a36cd24e806de2e3520b89f331ed8
minhi0449 <minhi0449@gmail.com>
2025-05-06 18:14:13 +0900

    fix: 사용자 입력 전 안내문구(a, b, c, d) 출력하도록 수정 / 불필요한 중복 입력 (sc.nextInt()) 제거

--- src/chap01/Max3.java
+++ src/chap01/Max3.java
@@ -1,15 +1,22 @@
 package chap01;
 
+// 전체 프로그램 앞머리 (클래스 선언보다 앞)에 작성합니다.
 import java.util.Scanner;
+// java.util 패키지에 있는 Scanner 클래스를 프로그램에 포함시킵니다.
 
 public class Max3 {
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
+        // main 메서드의 앞쪽(키보드 값을 입력 받는 'stdIn.nextInt()' 보다 앞에 작성합니다.
+        // System.in 은 키보드와 연결된 표준 입력 스트림(standard input stream)
 
         System.out.println("세 정수의 최댓값을 구합니다.");
         System.out.print("a의 값 : "); int a = stdIn.nextInt();
+        //                            --> 키보드로 입력한 값 중 정숫값을 읽어 들입니다.
         System.out.print("b의 값 : "); int b = stdIn.nextInt();
         System.out.print("c의 값 : "); int c = stdIn.nextInt();
+        //                            --> 키보드로 입력한 값 중 정숫값을 읽어 들이는 부분
+        //                            --> stdIn.nextInt() 를 호출하면 입력한 정숫값을 얻을 수 있음
 
         // a, b, c 의 최댓값을 구하여 max 에 대입합니다.
         int max = a;
