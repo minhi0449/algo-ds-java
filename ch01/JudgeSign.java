51887375b64e6ae71d7f6c066cb61caaf3ddb69c
minhi0449 <minhi0449@gmail.com>
2025-05-15 00:31:54 +0900

    refactor: ch 폴더를 root 폴더로 이동하여 학습 목차와 구조 통일

---
+++ ch01/JudgeSign.java
@@ -0,0 +1,30 @@
+package ch01;
+
+import java.util.Scanner;
+
+// 입력한 정숫값의 부호(양수/음수/0)를 판단하는 클래스
+class JudgeSign {
+    public static void main(String[] args) {
+        // Scanner : 사용자 입력을 처리하는 가장 기본적인 클래스
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println();
+        System.out.print("정수를 입력하세요. : ");
+
+        // 사용자로부터 입력받은 정수를 '변수 n'에 저장
+        int n = stdIn.nextInt(); // 다른 입력 메서드 : nextLine() 문자열 / nextDouble() 실수
+        // int : 정수형 데이터 (소수점 없는 숫자)
+
+        // n의 부호를 판단해 적절한 메시지 출력
+        if(n > 0) // n이 0보타 크면
+            System.out.println("이 수는 양수 입니다.");
+        else  if (n < 0) // n이 0보다 작으면
+            System.out.println("이 수는 음수 입니다.");
+        else // 위 조건이 모두 아니면 (n이 0이면)
+            System.out.println("이 수는 0 입니다.");
+
+        stdIn.close();
+
+    } // end of main
+} // end of class
+
---
+++ ch01/SumFor.java
@@ -0,0 +1,27 @@
+package ch01;
+
+import java.util.Scanner;
+
+// 실습 1-8
+// for 문으로 1, 2, ... , n의 합을 구함
+// 1부터 n까지 정수의 합을 for문으로 구하는 프로그램
+class SumFor {
+    public static void main(String[] args) {
+
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("1부터 n까지의 합을 구합니다.");
+        System.out.print("n값 : ");
+        int n = stdIn.nextInt();
+
+        int sum = 0;        // 합
+
+        for (int i = 1; i <= n; i++){
+            sum += i;       // sum 에 i 를 더함
+        }
+
+        System.out.println("1부터 " + n + "까지의 합은 '" + sum + "' 입니다");
+
+    }
+}
