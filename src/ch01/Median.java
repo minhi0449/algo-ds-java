ea795540d54667477be10f1df32771faea2cd23e
minhi0449 <minhi0449@gmail.com>
2025-05-14 23:12:53 +0900

    docs: Add inline comments for better readability (주석 추가로 가독성 향상)

--- src/ch01/Median.java
+++ src/ch01/Median.java
@@ -7,31 +7,37 @@ class Median {
 
     // 세 수의 중앙값을 구하는 함수
     static int med3(int a, int b, int c){
+        // a가 b보다 크거나 같을 때
         if (a >= b)
+            // b가 c보다 크거나 같으면 b가 중앙값
             if (b >= c)
                 return b;
+            // a가 c보다 작거나 같으면 b가 중앙값
             else if (a <= c)
                 return b;
+            // 그 외의 경우 c가 중앙값
             else
                 return c;
+        // a가 b보다 작고, a가 c보다 크면 a가 중앙값
         else if (a > c)
             return  a;
+        // a가 b보다 작고, b가 c보다 크면 c가 중앙값
         else if (b > c)
             return c;
+        // 그 외의 경우 b가 중앙값
         else
             return b;
-
-
 
     } // end of med3
 
     public static void main(String[] args) {
-
+        // 키보드 입력을 받기 위한 Scanner 객체 생성
         Scanner stdIn = new Scanner(System.in);
 
         System.out.println();
         System.out.println("---- 세 정수의 중앙값을 구합니다. ----");
 
+        // 사용자로부터 a, b, c 값을 입력 받음
         System.out.print("a의 값 : ");
         int a = stdIn.nextInt();
 
@@ -41,6 +47,7 @@ class Median {
         System.out.print("c의 값 : ");
         int c = stdIn.nextInt();
 
+        // 중앙값 계산 후 출력
         System.out.println("중앙값은 '" + med3(a, b, c) + "' 입니다.") ;
 
     } // end of main
