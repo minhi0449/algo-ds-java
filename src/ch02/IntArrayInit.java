6bec1299c8794b262ccecccc5ed1a95007aed147
minhi0449 <minhi0449@gmail.com>
2025-05-22 23:54:58 +0900

    feat(array): int형 배열 초기화 / for문 → 배열 요소 순차 출력

---
+++ src/ch02/IntArrayInit.java
@@ -0,0 +1,14 @@
+package ch02;
+
+// 실습 2-2
+// 구성 요소의 자료형이 int형인 배열 (구성 요솟수는 5: 배열 초기화에 의해 생성)
+class IntArrayInit {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5}; // 배열 초기화에 의해 생성
+
+        System.out.println();
+        for (int i = 0; i < a.length; i++){
+            System.out.println("a[" + i + "] = " + a[i]);
+        }
+    }
+}
