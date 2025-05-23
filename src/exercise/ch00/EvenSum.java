0177d313c2ac5295aa0f1c72453c12fc0bfcb27c
minhi0449 <minhi0449@gmail.com>
2025-05-23 10:21:44 +0900

    feat: for 루프와 modulo 연산자(%)를 사용하여 짝수 판별 및 누적 합계 계산

---
+++ src/exercise/ch00/EvenSum.java
@@ -0,0 +1,17 @@
+package exercise.ch00;
+
+// 1부터 1000까지 더할 때, 짝수만 더해서 결과 출력
+public class EvenSum {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println("1 ~ 1000까지 짝수의 합 : " + sum);
+
+    }
+}
