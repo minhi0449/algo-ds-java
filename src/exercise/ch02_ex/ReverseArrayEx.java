8cf61271110d2dd65e071139abe97490fea37570
minhi0449 <minhi0449@gmail.com>
2025-05-23 23:31:57 +0900

    feat(reverse): reverse() 함수로 역순 처리 과정 중 교환 로그를 출력하여 동작 흐름을 시각화

--- src/exercise/ch02_ex/ReverseArrayEx.java
+++ src/exercise/ch02_ex/ReverseArrayEx.java
@@ -1,5 +1,8 @@
 package exercise.ch02_ex;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 // 연습 2-2
 // 배열 요소에 값을 입력 받아 역순으로 정렬합니다.(과정을 출력)
 class ReverseArrayEx {
@@ -9,5 +12,31 @@ class ReverseArrayEx {
         int t = a[idx1]; a[idx1] = a[idx2]; a[idx2] = t;
     }
 
+    // --- 배열 a의 요소를 역순으로 정렬 --- //
+    static void reverse(int[] a) {
+        System.out.println(Arrays.toString(a));
+        for (int i = 0; i < a.length / 2; i++) {
+            System.out.println("a[" + i + "] 와 a[" + (a.length - i - 1) + "]을 교환합니다.");
+            swap(a, i, a.length - i - 1);
+            System.out.println(Arrays.toString(a));
+        }
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("요솟수는 : ");
+        int num = sc.nextInt();
+
+        int[] x = new int[num];
+
+        for (int i = 0; i < num; i++){
+            System.out.print("x[" + i + "] : ");
+            x[i] = sc.nextInt();
+        }
+
+        reverse(x);
+
+        System.out.println("----- 역순 정렬을 마쳤습니다. -----");
+    }
 }
