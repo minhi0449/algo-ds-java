0a525fd7f9f87314d3e1a7d074cb457317ee638f
minhi0449 <minhi0449@gmail.com>
2025-07-04 22:25:43 +0900

    클래스 X의 내부에서 COMPARATOR 정의

---
+++ ch03/src/X.java
@@ -0,0 +1,17 @@
+//import java.util.Comparator;
+//
+//// 실습 3-7
+//// 클래스 X의 내부에서 COMPARATOR 를 정의하는 방법
+//class X {
+//    // 필드, 메서드 등
+//    public static final Comparator<T> COMPARATOR = new Comp();
+//
+//    private static class Comp implements Comparator<T> {
+//        public int compare(T d1, T d2){
+//            // d1이 d2 보다 크면 양수 반환
+//            // d1이 d2 보다 작으면 음수 반환
+//            // d1이 d2 와 같으면 0 반환
+//
+//        }
+//    }
+//}
