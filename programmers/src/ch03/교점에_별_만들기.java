ba65b4a3b65059a5a370ab37ec4320f2c8f59fac
minhi0449 <minhi0449@gmail.com>
2025-08-20 16:56:50 +0900

    교점에 별 만들기

---
+++ programmers/src/ch03/교점에_별_만들기.java
@@ -0,0 +1,12 @@
+package ch03;
+
+public class Solution {
+    private static class Point {
+        public final long x, y;
+        private Point(long x, long y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+
