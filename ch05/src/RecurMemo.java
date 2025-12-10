3a6794173d216dac2c0da87a86ab77f777e2dab0
minhi0449 <minhi0449@gmail.com>
2025-12-10 23:48:18 +0900

    동일한 계산을 반복x

--- ch05/src/RecurMemo.java
+++ ch05/src/RecurMemo.java
@@ -7,7 +7,17 @@ public class RecurMemo {
     // 메모화를 도입한 recur 메서드
     static void recur(int n){
         if(memo[n + 1] != null){
-            System.out.println(memo[n + 1]);
+            System.out.println(memo[n + 1]); // 메모를 출력
+        } else {
+            if (n > 0) {
+                recur(n - 1);
+                System.out.println(n);
+                recur(n - 2);
+                memo[n + 1] = memo[n] + n + "\n" + memo[n - 1]; // 메모화
+            }
+            else {
+                memo[n + 1] = "";
+            }
         }
     }
 }
